49af0e1f23074c8a90b0fbf3eb712d79a6e8ca2b
AllenChiu <ghqzpm@gmail.com>
2023-09-13 23:36:28 +0800

    FIrst Commit

---
+++ src/main/java/com/bleach/Bleach/BleachApplication.java
@@ -0,0 +1,13 @@
+package com.bleach.Bleach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BleachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bleach/Bleach/BleachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bleach.Bleach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BleachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
